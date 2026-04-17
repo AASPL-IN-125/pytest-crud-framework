d03fee0d84e68300a221f9255a6091282d950e3c
AASPL-IN-125 <rushikesh.shinde@alignedautomation.com>
2026-04-17 11:39:26 +0530

    updated payload code

--- src/helpers/payload_manager.py
+++ src/helpers/payload_manager.py
@@ -1,4 +1,4 @@
-
+import random
 
 # Payload for creating a new user
 create_user_payload = {
@@ -11,3 +11,33 @@ update_user_payload = {
     "name": "Updated Name",
     "email": "updated@test.com"
 }
+
+
+
+names = [
+    "John Doe",
+    "Jane Smith",
+    "Michael Johnson",
+    "Emily Davis",
+    "Robert Brown",
+    "Sophia Wilson",
+    "David Miller",
+    "Olivia Taylor",
+    "James Anderson",
+    "Emma Thomas"
+]
+
+def generate_user_payload():
+    name = random.choice(names)
+    
+    # short random number (2 digits)
+    random_num = random.randint(10, 99)
+
+    email = name.lower().replace(" ", ".") + f"{random_num}@yopmail.com"
+
+    payload = {
+        "name": name,
+        "email": email
+    }
+
+    return payload
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO) 
 from src.constants.api_constant import get_all_user_url, get_user_by_id_url, post_user_url, put_user_url, delete_user_url, headers
-from src.helpers.payload_manager import create_user_payload, update_user_payload   
+from src.helpers.payload_manager import create_user_payload, update_user_payload, generate_user_payload  
 from src.utils.utils import get_request, post_request, put_request, delete_request
 
 def test_get_all_users():
@@ -20,7 +20,9 @@ def test_get_user_by_id():
     logging.info(f"GET User by ID Response: {response.json()}")    
 
 def test_create_user():
-    response = post_request(post_user_url, data=create_user_payload, headers=headers)
+    payload = generate_user_payload()
+    print(f"Generated Payload: {payload}")
+    response = post_request(post_user_url, data=payload, headers=headers)
     assert response.status_code == 201
     logging.info(f"POST Create User Response: {response.json()}")   
 
